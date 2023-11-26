9e2b79ad0e23052a3f052abc6118b28cf94d1a86
abhijeet0892 <abhijeetchougule1008@gmail.com>
2023-11-26 16:22:47 +0530

    converted class bassed to function based component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,58 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imgUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const NewsItem = (props) => {
+  let { title, description, imgUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span
+            className="badge bg-danger"
+            style={{ left: "90%", zIndex: "1" }}
           >
-            <span
-              className="badge bg-danger"
-              style={{ left: "90%", zIndex: "1" }}
-            >
-              {source}
-            </span>
-          </div>
-          <img
-            src={
-              !imgUrl
-                ? "https://elegalmetrology.jharkhand.gov.in/japnet/images/news.jpg"
-                : imgUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "Unknown" : author} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+            {source}
+          </span>
+        </div>
+        <img
+          src={
+            !imgUrl
+              ? "https://elegalmetrology.jharkhand.gov.in/japnet/images/news.jpg"
+              : imgUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
